ece578f718a497e77f558d695f361804ca9f2eed
kannan19302 <kannan19302@gmail.com>
2026-08-08 10:45:37 +0530

    feat(track-b): complete Track B design system (B01-B24)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../../ui-*/src/**/*.stories.@(ts|tsx)', '../../ui/src/**/*.stories.@(ts|tsx)'],
+  stories: [
+    '../../unierp-design-system/src/**/*.stories.@(ts|tsx)',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-links', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
